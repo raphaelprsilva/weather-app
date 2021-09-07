a0e16527274e1bc747c25433dc9dc31b92eb8a1c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-07 16:29:30 -0300

    Change http to https

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,10 @@
 const APIKey = '004dTU1swCv8D9SGILWREoq0Yv33s8fB';
 
 const getCityUrl = (cityName) => 
-  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}&lang=pt-br`;
+  `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}&lang=pt-br`;
 
 const getCityWeatherUrl = (key) =>
-  `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${APIKey}&language=pt-br`;
+  `https://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${APIKey}&language=pt-br`;
 
 const fetchData = async (url) => {
   try {
