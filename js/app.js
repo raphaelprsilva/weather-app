d776262431f21ae8aa829af1dade70318c0a71c6
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-07 15:37:15 -0300

    Refactor functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 const weatherForm = document.querySelector('[data-js="change-location"]');
+const weatherContainer = document.querySelector('[data-js="weather-container"]');
 const cityName = document.querySelector('[data-js="city-name"]');
 const temperatureValue = document.querySelector('[data-js="weather-value"]');
 const weatherText = document.querySelector('[data-js="weather-text"]');
 const weatherImage = document.querySelector('[data-js="time"]');
 const weatherIcon = document.querySelector('[data-js="time-icon"]');
 
-weatherForm.addEventListener('submit', async (event) => {
+const setWeatherImageSrc = (param) => {
+  param ? weatherImage.src = './src/day.svg' : weatherImage.src = './src/night.svg';
+};
+
+const setAllInnerHtml = (localizedName, weatherTextValue, temperature) => {
+  cityName.innerHTML = localizedName;
+  weatherText.innerHTML = weatherTextValue;
+  temperatureValue.innerHTML = temperature.Metric.Value;
+};
+
+const renderWeatherData = async (event) => {
   event.preventDefault();
 
   const cityInput = event.target.city.value;
@@ -16,15 +27,13 @@ weatherForm.addEventListener('submit', async (event) => {
   const weatherData = await fetchData(getCityWeatherUrl(Key));
   const [{ IsDayTime, Temperature, WeatherIcon, WeatherText }] = weatherData;
 
-  cityName.innerHTML = LocalizedName;
-  weatherText.innerHTML = WeatherText;
-  temperatureValue.innerHTML = Temperature.Metric.Value;
-
-  IsDayTime ? weatherImage.src = './src/day.svg' : weatherImage.src = './src/night.svg';
+  setAllInnerHtml(LocalizedName, WeatherText, Temperature);
+  setWeatherImageSrc(IsDayTime);
 
   weatherIcon.innerHTML = `<img src="./src/icons/${WeatherIcon}.svg" />`;
-  weatherIcon.insertAdjacentElement('beforeend', weatherIconImage);
 
-  console.log(weatherData);
+  weatherContainer.classList.remove('d-none');
   weatherForm.reset();
-});
+}
+
+weatherForm.addEventListener('submit', renderWeatherData);
