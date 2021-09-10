f49932a12ad4eb09e28183acad69f7c2b27ffb31
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-10 09:13:41 -0300

    Change functions arg names

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 const weatherForm = document.querySelector('[data-js="change-location"]');
-const weatherContainer = document.querySelector('[data-js="weather-container"]');
+const weatherContainer = document.querySelector(
+  '[data-js="weather-container"]'
+);
 const cityName = document.querySelector('[data-js="city-name"]');
 const temperatureValue = document.querySelector('[data-js="weather-value"]');
 const weatherText = document.querySelector('[data-js="weather-text"]');
 const weatherImage = document.querySelector('[data-js="time"]');
 const weatherIcon = document.querySelector('[data-js="time-icon"]');
 
-const setWeatherImageSrc = (param) => {
-  param ? weatherImage.src = './src/day.svg' : weatherImage.src = './src/night.svg';
+const setWeatherImageSrc = (IsDayTime) => {
+  IsDayTime
+    ? (weatherImage.src = "./src/day.svg")
+    : (weatherImage.src = "./src/night.svg");
 };
 
-const setAllInnerHtml = (localizedName, weatherTextValue, temperature) => {
-  cityName.innerHTML = localizedName;
-  weatherText.innerHTML = weatherTextValue;
-  temperatureValue.innerHTML = temperature.Metric.Value;
+const setAllInnerHtml = (LocalizedName, WeatherTextValue, Temperature) => {
+  cityName.innerHTML = LocalizedName;
+  weatherText.innerHTML = WeatherTextValue;
+  temperatureValue.innerHTML = Temperature.Metric.Value;
 };
 
 const renderWeatherData = async (event) => {
@@ -32,8 +36,8 @@ const renderWeatherData = async (event) => {
 
   weatherIcon.innerHTML = `<img src="./src/icons/${WeatherIcon}.svg" />`;
 
-  weatherContainer.classList.remove('d-none');
+  weatherContainer.classList.remove("d-none");
   weatherForm.reset();
-}
+};
 
-weatherForm.addEventListener('submit', renderWeatherData);
+weatherForm.addEventListener("submit", renderWeatherData);
